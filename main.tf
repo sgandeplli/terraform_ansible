ea4cf347d18e4f84030a9044f9a5b4f30cee928c
sgandeplli <sgandepalli@softility.com>
2024-10-25 18:07:57 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -27,22 +27,20 @@ resource "google_compute_instance" "centos_vm" {
 }
 
 output "vm_ip" {
-  value = google_compute_instance.centos_vm.network_interface[0].access_config[0].nat_ip
+  value = google_compute_instance.centos_vm.network_interface.0.access_config.0.nat_ip
 }
 
 resource "null_resource" "update_inventory" {
   provisioner "local-exec" {
     command = <<EOT
-      INSTANCE_IDS=$(gcloud compute instance-groups list-instances apache-instance-group --zone us-central1-a --format="value(instance)")
-      echo 'all:' > /var/lib/jenkins/workspace/loadbalancer/inventory.gcp.yml
-      echo '  hosts:' >> /var/lib/jenkins/workspace/loadbalancer/inventory.gcp.yml
-      for INSTANCE_ID in \$INSTANCE_IDS; do
-        INSTANCE_IP=\$(gcloud compute instances describe \$INSTANCE_ID --zone us-central1-a --format="value(networkInterfaces[0].accessConfigs[0].natIP)")
-        echo "    web_\$INSTANCE_ID:" >> /var/lib/jenkins/workspace/loadbalancer/inventory.gcp.yml
-        echo "      ansible_host: \$INSTANCE_IP" >> /var/lib/jenkins/workspace/loadbalancer/inventory.gcp.yml
-        echo "      ansible_user: centos" >> /var/lib/jenkins/workspace/loadbalancer/inventory.gcp.yml
-        echo "      ansible_ssh_private_key_file: /root/.ssh/id_rsa" >> /var/lib/jenkins/workspace/loadbalancer/inventory.gcp.yml
-      done
+      INSTANCE_ID=${google_compute_instance.centos_vm.name}
+      INSTANCE_IP=${google_compute_instance.centos_vm.network_interface.0.access_config.0.nat_ip}
+      echo 'all:' > /var/lib/jenkins/workspace/git-terra-ans/inventory.gcp.yml
+      echo '  hosts:' >> /var/lib/jenkins/workspace/git-terra-ans/inventory.gcp.yml
+      echo "    web_$INSTANCE_ID:" >> /var/lib/jenkins/workspace/git-terra-ans/inventory.gcp.yml
+      echo "      ansible_host: $INSTANCE_IP" >> /var/lib/jenkins/workspace/git-terra-ans/inventory.gcp.yml
+      echo "      ansible_user: centos" >> /var/lib/jenkins/workspace/git-terra-ans/inventory.gcp.yml
+      echo "      ansible_ssh_private_key_file: /root/.ssh/id_rsa" >> /var/lib/jenkins/workspace/git-terra-ans/inventory.gcp.yml
     EOT
   }
 
